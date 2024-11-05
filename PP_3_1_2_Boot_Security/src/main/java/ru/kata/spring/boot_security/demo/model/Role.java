d5a8c9660ec95897c48b56876780a1285b020894
ev-rdkv <ev.rdkv@mail.ru>
2024-11-05 21:10:03 +0300

    update 5

--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.model;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "roles")
@@ -11,6 +12,7 @@ public class Role implements GrantedAuthority {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String role;
+
 
     public Role() {
 
